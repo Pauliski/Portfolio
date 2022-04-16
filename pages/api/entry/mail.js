e41071a95d675ff5be489c34a3c19dc324f83426
Pauliski <parokoola@gmail.com>
2022-04-16 06:09:53 +0100

    created .env file for secret credientials

--- pages/api/entry/mail.js
+++ pages/api/entry/mail.js
@@ -1,22 +1,20 @@
 import nodemailer from "nodemailer";
 import mailGun from "nodemailer-mailgun-transport";
-import fs from "fs";
-import path from "path";
-import Mustache from "mustache";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 
 const auth = {
   auth: {
-    api_key: "key-ab76b7592f7f89c5a212f9815fbae63a",
-    domain: "sandboxdd09af6158b148a6a91d7014ff571236.mailgun.org",
+    api_key: process.env.API_KEY,
+    domain: process.env.DOMAIN,
   },
 };
 
 const transporter = nodemailer.createTransport(mailGun(auth));
 
 const sendMail = (username, email, messageBody, cb) => {
-  console.log(path.join(__dirname, "email.html"));
-  // const emailTemplate = Mustache.render(htmlTemplate, { name: "Pauliski" });
   const mailOptions = {
     from: email,
     to: "arokoola92@yahoo.com",
